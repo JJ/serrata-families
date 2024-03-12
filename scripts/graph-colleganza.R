bdcaa0f3c82378f1db83f812f4227f0d64528803
JJ Merelo <jjmerelo@gmail.com>
2024-03-12 14:11:57 +0100

    #5 plot great council

--- scripts/graph-colleganza.R
+++ scripts/graph-colleganza.R
@@ -2,10 +2,8 @@ library(igraph)
 library(dupNodes)
 
 readRDS("../data/colleganza-graph.rds")
-# V(colleganza.graph)$betweenness <- betweenness(colleganza.graph)
+V(colleganza.graph)$betweenness <- betweenness(colleganza.graph)
 plot(colleganza.graph, vertex.size=V(colleganza.graph)$betweenness/100, edge.arrow.size=0.5, edge.curved=0.1, edge.color="grey", main="Colleganza graph")
-
-# select subgraph from colleganza.graph where betwenness > 0
 
 colleganza.subgraph <- induced_subgraph(colleganza.graph, V(colleganza.graph)[V(colleganza.graph)$betweenness > 0])
 plot(colleganza.subgraph, vertex.size=V(colleganza.subgraph)$betweenness/300, edge.arrow.size=0.5, edge.curved=0.1, edge.color="grey", main="Colleganza subgraph")
@@ -13,7 +11,9 @@ plot(colleganza.subgraph, vertex.size=V(colleganza.subgraph)$betweenness/300, ed
 readRDS("../data/colleganza-pairs-date.rds")
 colleganza.btw <- DNSL.betweenness(colleganza.pairs.date,first.node="Family1",second.node="Family2")
 
-# print colleganza.btw sorted by value
 print(colleganza.btw[order(colleganza.btw,decreasing=T)])
 
-# Assign blue color to nodes of families that belong to the great council
+readRDS("../data/great-council-families.rds")
+V(colleganza.graph)$color <- "yellow"
+V(colleganza.graph)[V(colleganza.graph)$name %in% great.council.families]$color <- "blue"
+plot(colleganza.graph, vertex.size=V(colleganza.graph)$betweenness/200, vertex.color=V(colleganza.graph)$color, edge.arrow.size=0.5, edge.curved=0.1, edge.color="grey", main="Colleganza graph")
