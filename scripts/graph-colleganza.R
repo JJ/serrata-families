866f69f3542dc18be1382a1f50c38b21709eb662
JJ Merelo <jjmerelo@gmail.com>
2024-03-13 18:39:41 +0100

    Plotting communities with simplified graph

--- scripts/graph-colleganza.R
+++ scripts/graph-colleganza.R
@@ -33,6 +33,22 @@ V(pre.serrata)$color <- "yellow"
 V(pre.serrata)[V(pre.serrata)$name %in% great.council.families]$color <- rgb(0.7,0,0,0.5)
 plot(pre.serrata, vertex.size=V(pre.serrata)$betweenness/200, vertex.color=V(pre.serrata)$color, edge.arrow.size=0.5, edge.curved=0.1, edge.color="grey", main="Colleganza graph, pre-serrata")
 
+
+
+pre.serrata.components <- igraph::components(pre.serrata, mode="weak")
+pre.serrata.biggest_cluster_id <- which.max(pre.serrata.components$csize)
+pre.serrata.vert_ids <- V(pre.serrata)[pre.serrata.components$membership == pre.serrata.biggest_cluster_id]
+
+connected.pre.serrata <- induced_subgraph(pre.serrata, pre.serrata.vert_ids)
+connected.simplified.pre.serrata <- simplify(connected.pre.serrata)
+
+E(connected.simplified.pre.serrata)$weight <- 1
+pro.communities.pre.serrata <- simplify(connected.simplified.pre.serrata,edge.attr.comb=list(weight="sum"))
+
+pre.serrata.communities <- cluster_edge_betweenness(pro.communities.pre.serrata)
+plot(pre.serrata.communities, pro.communities.pre.serrata, vertex.size=V(pre.serrata)$betweenness/200, vertex.color=V(pre.serrata)$color, edge.arrow.size=0.5, edge.curved=0.1, edge.color="grey", main="Colleganza communities, pre-serrata")
+
+
 post.serrata <- colleganza.slice(from=1310)
 V(post.serrata)$betweenness <- betweenness(post.serrata)
 V(post.serrata)$color <- "yellow"
