60f9e594fc732e22fa005c053021cbbbd4f7e6f7
JJ Merelo <jjmerelo@gmail.com>
2024-03-13 13:33:30 +0100

    #5 :white_check_mark:

--- R/colleganza.slice.R
+++ R/colleganza.slice.R
@@ -1,6 +1,6 @@
 require(igraph)
 data("colleganza.pairs.date")
-colleganza.slice <- function( from=1173,to=1342) {
+colleganza.slice <- function( from=1073,to=1342) {
   colleganza.pairs.slice <- colleganza.pairs.date[ colleganza.pairs.date$date >= from & colleganza.pairs.date$date <= to,]
   return( graph_from_data_frame(colleganza.pairs.slice, directed=F))
 }
--- tests/testthat/test-colleganza.slice.R
+++ tests/testthat/test-colleganza.slice.R
@@ -5,6 +5,11 @@ all.vertices <- length(V(default.graph))
 
 test_that("colleganza dataset is sliced correctly", {
   all.contracts <- colleganza.slice()
-  expect_is(all.contracts, "igraph")
+  expect_type(all.contracts, "list")
   expect_equal(length(V(all.contracts)), all.vertices)
+
+  pre.serrata.contracts <- colleganza.slice(to=1261)
+  expect_lt(length(V(pre.serrata.contracts)), all.vertices)
+  post.serrata.contracts <- colleganza.slice(from=1310)
+  expect_lt(length(V(post.serrata.contracts)), all.vertices)
 })
