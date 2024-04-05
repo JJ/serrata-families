67b035d5f150acb317d9e839049519be6e8da3b0
JJ Merelo <jjmerelo@gmail.com>
2024-04-05 14:28:59 +0200

    Adapt to new data files

--- scripts/graph-colleganza.R
+++ scripts/graph-colleganza.R
@@ -2,19 +2,19 @@ require(igraph)
 require(dupNodes)
 require(serrata.families)
 
-readRDS("../data/colleganza-graph.rds")
+load("../data/colleganza.graph.rda")
 V(colleganza.graph)$betweenness <- betweenness(colleganza.graph)
 plot(colleganza.graph, vertex.size=V(colleganza.graph)$betweenness/100, edge.arrow.size=0.5, edge.curved=0.1, edge.color="grey", main="Colleganza graph")
 
 colleganza.subgraph <- induced_subgraph(colleganza.graph, V(colleganza.graph)[V(colleganza.graph)$betweenness > 0])
 plot(colleganza.subgraph, vertex.size=V(colleganza.subgraph)$betweenness/300, edge.arrow.size=0.5, edge.curved=0.1, edge.color="grey", main="Colleganza subgraph")
 
-readRDS("../data/colleganza-pairs-date.rds")
+load("../data/colleganza.pairs.date.rda")
 colleganza.btw <- DNSL.betweenness(colleganza.pairs.date,first.node="Family1",second.node="Family2")
 
 print(colleganza.btw[order(colleganza.btw,decreasing=T)])
 
-readRDS("../data/great-council-families.rds")
+load("../data/great.council.families.rda")
 V(colleganza.graph)$color <- "yellow"
 V(colleganza.graph)[V(colleganza.graph)$name %in% great.council.families]$color <- rgb(0.7,0,0,0.5)
 plot(colleganza.graph, vertex.size=V(colleganza.graph)$betweenness/200, vertex.color=V(colleganza.graph)$color, edge.arrow.size=0.5, edge.curved=0.1, edge.color="grey", main="Colleganza graph")
