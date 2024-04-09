2cb93541547d3689ecf467d6dffd9ce13d84da61
JJ Merelo <jjmerelo@gmail.com>
2024-04-09 10:12:08 +0200

    #11 documenting the data (also #8)

--- scripts/generate-Rda.R
+++ scripts/generate-Rda.R
@@ -16,3 +16,7 @@ use_data(colleganza.graph, overwrite=T)
 great.council.families <- fromJSON("../data-raw/great-council-families.json")
 use_data(great.council.families)
 
+great.council.families.dates <- read.csv("../data-raw/families-great-council-date.csv",sep=";")
+great.council.families.dates$Start <- as.numeric(trimws(great.council.families.dates$Start))
+great.council.families.date <- great.council.families.dates[ !is.na(great.council.families.dates$Start),]
+use_data(great.council.families.date, overwrite=T)
