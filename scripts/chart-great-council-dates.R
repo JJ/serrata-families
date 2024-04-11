aa1cf8f2be77cdea85e856272777067d71c8ffa6
JJ Merelo <jjmerelo@gmail.com>
2024-04-11 14:39:58 +0200

    Firs exploration of intersection

--- scripts/chart-great-council-dates.R
+++ scripts/chart-great-council-dates.R
@@ -1,4 +1,6 @@
+library(serrata.families)
 library(ggplot2)
+library(igraph)
 
 load("../data/great.council.families.date.rda")
 
@@ -7,3 +9,8 @@ ggplot(great.council.families.date, aes(x=Start)) +
   labs(title="Great Council Families Dates", x="Date", y="Count") +
   theme_minimal() +
   theme(plot.title = element_text(hjust = 0.5))
+
+families.enlargement <- great.council.families.date[great.council.families.date$Start >= 1297 & great.council.families.date$Start <= 1330,]$Family
+
+contracts.pre.1261 <- colleganza.slice(to=1261)
+families.colleganza <- V(contracts.pre.1261)$name
