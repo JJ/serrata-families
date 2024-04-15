96c97b38f9257e42e5bf5d21c08f9c6200977bb5
JJ Merelo <jjmerelo@gmail.com>
2024-04-15 14:14:05 +0200

    #16 Trying to plot

---
+++ scripts/colleganza-type-contracts-families.R
@@ -0,0 +1,6 @@
+library(ggplot2)
+library(tidyverse)
+
+colleganza.family.contract.types <- read.csv("data-raw/contract-family-year.csv",header=T)
+
+ggplot(colleganza.family.contract.types, aes(x=Family, y=Year, color=Contract.Type, group=Family)) + geom_point() + geom_line() + theme_minimal() + labs(title="Colleganza Family Contracts", x="Year", y="Contract Type") + geom_label( data=filter(colleganza.family.contract.types, Year==max(Year)), aes(label=Family), nudge_x=0.5, nudge_y=0.5)
