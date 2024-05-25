d57d09fc7a77c17b610d563d7595a4e26698241c
JJ Merelo <jjmerelo@gmail.com>
2024-05-25 14:07:51 +0200

    Adding changepoint analysis
    Mainly to take into account one-sided and two-sided colleganza

---
+++ scripts/changepoint.R
@@ -0,0 +1,17 @@
+library(trend)
+library(dplyr)
+library(ggplot2)
+
+load("data/colleganza.pairs.date.rda")
+
+colleganza.pairs.date %>% group_by(date) %>% summarise(Contracts=n()) -> contracts.per.year
+ggplot(contracts.per.year,aes(x=date,y=Contracts))+geom_point()
+
+contracts.pre.serrata <- contracts.per.year[contracts.per.year$date <= 1261,]
+
+lanzante.contracts <- lanzante.test(contracts.pre.serrata$Contracts)
+
+contracts.pre.serrata[lanzante.contracts$estimate,]
+
+contracts.pre.changepoint <- colleganza.pairs.date[ colleganza.pairs.date$date <= 1197,]
+
