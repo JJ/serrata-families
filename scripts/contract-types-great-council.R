5b194b285c19bcec94fafc5810ca34d4f4699d4c
JJ Merelo <jjmerelo@gmail.com>
2024-10-12 09:06:50 +0200

    The number of contracts of tractors and stans is not significantly different

--- scripts/contract-types-great-council.R
+++ scripts/contract-types-great-council.R
@@ -11,17 +11,26 @@ print(contract.data.families[ contract.data.families$great.council == FALSE & co
 
 ggplot( contract.data.families, aes(x=Role, y=Last.Year, shape=great.council, size=Total.Contracts)) +
   geom_jitter(aes(color=great.council)) +
-  labs(title="Great Council Families in Contracts", x="Family", y="Count")
+  labs(title="Great Council Families in Contracts", x="Family", y="Year of last contract")
 
 ggplot( contract.data.families, aes(x=Last.Year,y=Total.Contracts, shape=great.council,color=Role)) +
   geom_point(size=3,alpha=0.7) +
   theme(axis.text.x = element_text(angle = 90, hjust = 1)) +
-  labs(title="Great Council Families in Contracts", x="Year", y="Count")
+  labs(title="Great Council Families in Contracts", x="Year of last contract", y="Total contracts")
 
 ggplot( contract.data.families, aes(x=Role, y=Total.Contracts)) +
   geom_jitter(aes(shape=great.council)) + geom_boxplot() +
-  labs(title="Great Council Families in Contracts", x="Role", y="Count") +
+  labs(title="Great Council Families in Contracts", x="Role", y="Number of contracts") +
   theme(axis.text.x = element_text(angle = 90, hjust = 1))
+
+significant.differences.both.stan <- wilcox.test( contract.data.families[ contract.data.families$Role == "both", ]$Total.Contracts,
+                                        contract.data.families[ contract.data.families$Role == "stan", ]$Total.Contracts, alternative="greater" )
+
+significant.differences.both.tractor <- wilcox.test( contract.data.families[ contract.data.families$Role == "both", ]$Total.Contracts,
+                                                  contract.data.families[ contract.data.families$Role == "tractor", ]$Total.Contracts, alternative="greater" )
+
+significant.differences.tractor.stan <- wilcox.test( contract.data.families[ contract.data.families$Role == "tractor", ]$Total.Contracts,
+                                                  contract.data.families[ contract.data.families$Role == "stan", ]$Total.Contracts )
 
 ggplot( contract.data.families, aes(x=Role, y=Last.Year)) +
   geom_jitter(aes(shape=great.council)) + geom_boxplot(notch=T) +
